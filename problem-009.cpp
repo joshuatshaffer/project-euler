687e29579c47d6c012e6fd669e7520aafe418c89
Joshua Shaffer <joshuatshaffer@gmail.com>
2016-02-07 13:24:14 -0600

    solved problem 9

---
+++ problem-009.cpp
@@ -0,0 +1,26 @@
+//  Problem 8
+//  Created by Joshua on 2/7/16.
+//  Copyright © 2016 joshuatshaffer. All rights reserved.
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    for (int a=0; a<1001; ++a) {
+        for (int b=0; b<1001; ++b) {
+            for (int c=0; c<1001; ++c) {
+                if (
+                    (a < b && b < c) &&
+                    (a*a+b*b == c*c) &&
+                    (a+b+c == 1000)
+                    ) {
+                    cout << " a = " << a << endl;
+                    cout << " b = " << b << endl;
+                    cout << " c = " << c << endl;
+                    cout << " abc = " << (a*b*c) << endl;
+                   // return 0;
+                }
+            }
+        }
+    }
+    return 0;
+}

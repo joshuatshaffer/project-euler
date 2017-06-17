3966e327f0ca5b805ce8e88494fe025415260a20
joshuatshaffer <joshuatshaffer@gmail.com>
2017-06-17 09:42:42 -0500

    Reorganized into seperate dirs.

--- 022/main.hs
+++ 022/main.hs
@@ -7,6 +7,6 @@ alphVal = sum . map (\c -> fromEnum c - fromEnum 'A' + 1)
 
 main :: IO ()
 main = do
-  s <- readFile "022-names.txt"
+  s <- readFile "names.txt"
   let names = read $ "[" ++ s ++ "]"
   print . sum . zipWith (*) [1..] . map alphVal . sort $ names
